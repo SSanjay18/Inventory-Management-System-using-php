70ee11bd929a245710b6959f7dbabfbdbc85df41
SSanjay18 <103240670+SSanjay18@users.noreply.github.com>
2023-09-26 12:04:04 +0530

    Add files via upload

---
+++ inc/config/constants.php
@@ -0,0 +1,21 @@
+<?php
+	// Root url for the site
+	define('ROOT_URL', 'http://localhost/inventory-management-system/');
+	
+	
+	// Database parameters
+	// Data source name
+	define('DSN', 'mysql:host=localhost:3300;dbname=shop_inventory');
+	
+	// Hostname
+	define('DB_HOST', 'localhost:3300');
+	
+	// DB user
+	define('DB_USER', 'root');
+	
+	// DB password
+	define('DB_PASSWORD', 'root');
+	
+	// DB name
+	define('DB_NAME', 'shop_inventory');
+?>
